ee10a9cfcd4e28dfe55bce9ca3fb8138f2a7d21d
fannanqi <1773252187@qq.com>
2023-06-02 12:57:50 +0800

    thend

---
+++ src/test/java/com/jdbc/DemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.jdbc;
+
+import com.jdbc.config.JDBCUtils_Druid;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class DemoApplicationTests {
+    @Autowired DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+    }
+
+}
